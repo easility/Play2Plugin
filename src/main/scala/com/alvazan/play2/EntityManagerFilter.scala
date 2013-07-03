4d5d970b24d8f7cd28943384fd80dcc6fdbb803d
Radim Kolar <hsn@sendmail.cz>
2013-07-03 04:43:20 +0200

    support em automatic binding to local thread

--- src/main/scala/com/alvazan/play2/EntityManagerFilter.scala
+++ src/main/scala/com/alvazan/play2/EntityManagerFilter.scala
@@ -1,6 +1,7 @@
 package com.alvazan.play2
 
 import play.api.mvc._
+import play.api.libs.concurrent.Execution.Implicits._
 
 object EntityManagerFilter extends Filter {
    def apply(next: (RequestHeader) => Result)(rh: RequestHeader):Result = {
@@ -8,12 +9,19 @@ object EntityManagerFilter extends Filter {
       val factory = NoSqlForPlay2.getEntityManagerFactory()
       if (factory != null) {
          // bind it to current thread
+         println("binding em to thread")
          NoSqlForPlay2.bindForCurrentThread(factory.createEntityManager())
       }
       // call next filter in chain
-      val rc = next(rh)
-      // unbind entity manager from current thread
-      NoSqlForPlay2.clearContext()
-      rc
+      next(rh) match {
+         case plain: PlainResult => println("got plain result"); 
+                                    NoSqlForPlay2.clearContext(); 
+                                    plain
+         case async: AsyncResult => println("got async result");
+                                    async.transform( ( f:PlainResult ) => { println("unbinding")
+                                                              NoSqlForPlay2.clearContext();
+                                                              f } 
+                                                   )
+      }
    }
 }
