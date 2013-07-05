027a2d65174fd7f40567d8f1830e6319c32d2ea8
Radim Kolar <hsn@sendmail.cz>
2013-07-06 00:15:42 +0200

    remove Filter and replace it with ActionHelper

---
+++ src/main/scala/com/alvazan/play2/ActionHelpers.scala
@@ -0,0 +1,14 @@
+package com.alvazan.play2
+
+import play.api.mvc._
+import com.alvazan.orm.api.base.NoSqlEntityManager
+
+object ActionHelpers {
+
+  def withEntityManager(action: NoSqlEntityManager => EssentialAction):EssentialAction = {
+
+     EssentialAction { request => 
+         action(NoSqlForPlay2.getEntityManagerFactory().createEntityManager())(request)
+     }
+  }
+}
